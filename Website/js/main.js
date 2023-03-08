780d1a3c28c8520e606d343440893d007aa7c5fc
Dane <daneb@broadbandtap.co.uk>
2023-03-08 23:29:58 +0000

    Updated donate SWAL & footer links

--- Website/js/main.js
+++ Website/js/main.js
@@ -1,18 +1,18 @@
 function donateMsg() {
     Swal.fire({
-        title: '<b style="color: white;">Buy me a coffee!',
+        title: '<b style="color: black;">Buy me a beer! <i class="fa-solid fa-beer-mug-empty"></i>',
         icon: 'none',
         html:
-          '<p style="color: white;">If you enjoy the projects on my site and would like to support me in any way, please consider donating to me below. Donations are not expected at all but are very much appreciated! :)</p>' +
-          '<p><a style="color: #0079C1;" href="https://paypal.me/daaane" style="text-decoration: none;"><b><i class="fa-brands fa-paypal"></i> PayPal</b></a></p>' +
-          '<p><a style="color: #00C244;" href="https://cash.app/$daneffx" style="text-decoration: none;"><b><i class="fa-solid fa-dollar-sign"></i> Cash App</b></a></p>' +
-          '<p style="color: #f7931a;"><b><i class="fa-brands fa-bitcoin"></i> <u>BITCOIN:</u> <i style="color: white;">bc1qyevcm5ct2e238fdc44jau46galpq6xxd5xdxuj</i></b></p>' +
-          '<p style="color: #a05cff;"><b><i class="fa-brands fa-ethereum"></i> <u>ETHEREUM:</u> <i style="color: white;">0x7e31dF204247A3b9661D109783218e231959a1E4</i></b></p>' +
-          '<p style="color: #2a71d0;"><b><i class="fa-solid fa-coins"></i> <u>CARDANO:</u> <i style="color: white;">addr1qxy49lgx9mnqts0f3kwxm2ltr3qfuxngh49pk5kacqhx7sdg4gch48g35fap6a4y3fup3pzjam06yukhnzatdlktghdq9unjew</i></b></p>' +
-          '<p style="color: white;">Thank you!</p>',
+          '<p style="color: black; font-size: 12px;">If you enjoy the projects on my site and would like to support me in any way, please consider donating to me below. Donations are not expected at all but are very much appreciated! </p>' +
+          '<p><a style="color: #0079C1; font-size: 13px; text-decoration: none;" href="https://paypal.me/daaane"><b><i class="fa-brands fa-paypal"></i> PayPal</b></a></p>' +
+          '<p><a style="color: #00C244; font-size: 13px; text-decoration: none;" href="https://cash.app/$daneffx" style="text-decoration: none;"><b><i class="fa-solid fa-dollar-sign"></i> Cash App</b></a></p>' +
+          '<p style="color: #f7931a; font-size: 13px; text-decoration: none;"><b><i class="fa-brands fa-bitcoin"></i> BITCOIN <br> <i style="color: black; font-size: 11px;">bc1qyevcm5ct2e238fdc44jau46galpq6xxd5xdxuj</i></b></p>' +
+          '<p style="color: #a05cff; font-size: 13px; text-decoration: none;"><b><i class="fa-brands fa-ethereum"></i> ETHEREUM <br> <i style="color: black; font-size: 11px;">0x7e31dF204247A3b9661D109783218e231959a1E4</i></b></p>' +
+          '<p style="color: #2a71d0; font-size: 13px; text-decoration: none;"><b><i class="fa-solid fa-coins"></i> CARDANO <br> <i style="color: black; font-size: 11px;">addr1qxy49lgx9mnqts0f3kwxm2ltr3qfuxngh49pk5kacqhx7sdg4gch48g35fap6a4y3fup3pzjam06yukhnzatdlktghdq9unjew</i></b></p>' +
+          '<p style="color: black; font-size: 12px;"><b>Thank you! <i class="fa-regular fa-face-smile-beam"></i></b></p>',
         showCloseButton: false,
         showCancelButton: false,
-        background: '#363636',
+        background: '#ececec',
         confirmButtonColor: "#ffffff",
         confirmButtonText:
           'Close',
